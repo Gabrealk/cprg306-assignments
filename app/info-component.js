6bfa08fe28c6c9b8261d7c1acf3c1b4f3dec922d
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-09-21 11:43:06 -0600

    week2 submission

---
+++ app/info-component.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Geninfo(){
+    return(
+        <main>
+            <p>Name: Gabreal Kalaf</p>
+            <p>Course: CPRG 306-E</p>
+            <Link href="https://github.com/Gabrealk">My GitHub</Link>
+        </main>
+    )
+}
