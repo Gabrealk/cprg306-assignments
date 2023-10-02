ba704ad76b511cbfad2a6c46ea20bf41be30e05e
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-02 12:15:58 -0600

    y54a

--- app/week3/page.js
+++ app/week3/page.js
@@ -4,11 +4,17 @@ import Item from "./item";
 export default function Mainshoppingpage(){
     return(
         <main>
-            <h1>SHOPPING LIST: </h1>
-            <button>
+            <h1 className="font-bold ">SHOPPING LIST: </h1>
+            <Item />
+            <button className="font-bold 
+                px-7 py-2 
+                text-black 
+                bg-blue-700 
+                border-2 border-black m-4 rounded 
+                hover:bg-blue-500">
                 <p><Link href ="../">Go Back</Link></p>
             </button>
-            <Item />
+            
             
         </main>
 
