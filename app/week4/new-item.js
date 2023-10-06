9e7d6c487eff75bf49d7d153d47c5e94dbe923fa
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-06 11:24:46 -0600

    new functions updated, roughly works,needs styling

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -27,8 +27,6 @@ export default function Newevent(){
         //backticks ``` are used for implementing literals, or embedded expressions
         //window alert when submitted pre sure
         window.alert(`Item: ${Items.name}, Quantity: ${Items.quantity}, Category: ${Items.category}`);
-        
-
       };
 
 
@@ -37,16 +35,45 @@ export default function Newevent(){
     //stuff in the return/main field is html/css etc
     return(
         <main>
+            <form>
+                {/*used to input the items name and updates the value
+                based on the input*/}
+                <label>Item Name:</label>
+                <input type="text" 
+                value={name} placeholder="Item name"
+                onChange={(nchange) => setName(nchange.target.value)}
+                required //makes the name field required in order to submit the form
+                color="black"
+                />
+                {/*the change basically represents he event object */}
 
-            
-            <input type="text" 
-            value={name} placeholder="Item name"
-            onChange={(change) => setName(change.target.value)} 
-            />
+                <label>Quantity:</label>
+                <input type="number"
+                value={quantity} min={1} max={99}
+                onChange={(qchange) => setName(qchange.target.value)}
+                required
+                //same dealio as before
+                />
 
+                <label>Category:</label>
+                <select value={category} onChange={(cchange) => setName(cchange.target.value)}>
+                    <option value="produce">produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Frozen Foods">Frozen Foods</option>
+                    <option value="Canned Goods">Canned Goods</option>
+                    <option value="Dry Goods">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="household">produce</option>
+                    <option value="Other">Other</option>
+                </select>
+                {/*above select statement gives the user different options
+                 to choose from with the variables stated*/}
 
-            <form onSubmit="submitFunction(event)">
-                <button type="submit">Submit</button>
+                <button type="submit">Add Item</button>
+
             </form>
 
         </main>
