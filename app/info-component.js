4bd83848940d4d27a3280a10cbcd986b6e24e40f
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-09 15:14:28 -0600

    github button addition

--- app/info-component.js
+++ app/info-component.js
@@ -5,7 +5,16 @@ export default function Geninfo(){
         <main>
             <p>Name: Gabreal Kalaf</p>
             <p>Course: CPRG 306-E</p>
-            <Link href="https://github.com/Gabrealk">My GitHub</Link>
+            <div>
+                <button className="font-bold 
+                        px-7 py-2 
+                        text-black 
+                        bg-blue-700 
+                        border-2 border-black m-4 rounded 
+                        hover:bg-blue-500">
+                <p><Link href="https://github.com/Gabrealk">My GitHub</Link></p>
+                </button>
+            </div>
         </main>
     )
 }
