0fa4cf202db97cf0b04ab821e2c3b76cd99f4a0a
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-09 15:12:43 -0600

    week5 folder creation

--- app/page.js
+++ app/page.js
@@ -42,6 +42,17 @@ export default function Home()
         </button>
       </div>
 
+      <div>
+        <button className="font-bold 
+                px-7 py-2 
+                text-black 
+                bg-blue-700 
+                border-2 border-black m-4 rounded 
+                hover:bg-blue-500">
+          <p><Link href="week5">Week 5 *IN PROGRESSS*</Link></p>
+        </button>
+      </div>
+
     </main>
 
 
