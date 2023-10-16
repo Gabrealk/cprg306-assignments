5bba2494c4f460114b3773a69d077b9c7f28834e
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-15 20:35:32 -0600

    styling updates!

--- app/week5/item.js
+++ app/week5/item.js
@@ -1,8 +1,11 @@
 
-
+//Updated item function, where the whole item is declared , and the styling is input here rather than repeating it
+//updated through learning from week 5! and communicating with different classmates to improve my efficiency and cleanliness through suggestions and tips
 export default function Item ({item}) {
     return(
-        <main>
+        <main className="font-bold px-7 py-2 text-black bg-green-400 
+        border-2 border-black m-4 rounded hover:bg-green-700">
+            
             <h1>{item.name}</h1>
             <p>The amount of items there are is: {item.quantity}</p>
             <p>The Item belongs in the {item.category} category</p>
