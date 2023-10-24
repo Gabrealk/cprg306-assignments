72f53c36f9cca7edab5d9bf4ffa2010f5c780411
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-10-24 08:54:12 -0600

    week 7 link to page.js

--- app/page.js
+++ app/page.js
@@ -64,6 +64,17 @@ export default function Home()
         </button>
       </div>
 
+      <div>
+        <button className="font-bold 
+                px-7 py-2 
+                text-black 
+                bg-blue-700 
+                border-2 border-black m-4 rounded 
+                hover:bg-blue-500">
+          <p><Link href="week7">Week 7 **In Construction**</Link></p>
+        </button>
+      </div>
+
     </main>
 
 
