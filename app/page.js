8f7add349227958ad890501133259410990f5c60
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-11-01 19:00:07 -0600

    week 8 file imports

--- app/page.js
+++ app/page.js
@@ -75,6 +75,17 @@ export default function Home()
         </button>
       </div>
 
+      <div>
+        <button className="font-bold 
+                px-7 py-2 
+                text-black 
+                bg-blue-700 
+                border-2 border-black m-4 rounded 
+                hover:bg-blue-500">
+          <p><Link href="week8">Week 8</Link></p>
+        </button>
+      </div>
+
     </main>
 
 
